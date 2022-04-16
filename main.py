591356e20a914e8b0177a54ece9527424340a8bd
Cheung-KaWai <kawaicheungdev@gmail.com>
2022-04-16 09:59:20 +0200

    Add scheduled message

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import os
 from dotenv import load_dotenv
 from discord.ext import tasks, commands
-import datetime
+import datetime, asyncio,requests,json
 
 load_dotenv()
 
 bot = commands.Bot(command_prefix="!")
-global channel
+vacation = datetime.date(2022,6,24)
+links=json.load(open("gifs.json"))
 
-@bot.event
-async def on_ready():
-  channel = bot.get_channel(729276302443413577)
-  print("bot is ready")
-  await channel.send("Alo")
+def getDays():
+  today = datetime.date.today()
+  diff = vacation - today
+  return diff.days
+
+async def schedule_daily_message():
+  while True:
+    now = datetime.datetime.now()
+    then = now + datetime.timedelta(days=1)
+    then.replace(hour=8,minute=0)
+    wait_time=(then-now).total_seconds()
+    await asyncio.sleep(wait_time)
+
+    channel = bot.get_channel(729276302443413577)
+
+    await channel.send(f"nog {getDays()} dagen SIUUUUUUUUU")
+    await channel.send(links["gif"])
+
 
 @bot.command()
 async def alo(ctx):
@@ -20,9 +34,13 @@ async def alo(ctx):
 
 @bot.command()
 async def vakantie(ctx):
-  today = datetime.date.today()
-  future = datetime.date(2022,6,24)
-  diff = future - today
-  await ctx.send(f"Nog {diff.days} dagen SIUUUUUUUUU")
+  await ctx.send(f"Nog {getDays()} dagen SIUUUUUUUUU")
+
+@bot.event
+async def on_ready():
+  channel = bot.get_channel(729276302443413577)
+  print("bot is ready")
+  await channel.send("Alo SIUUUUUUUUU")
+  await schedule_daily_message()
 
 bot.run(os.getenv('TOKEN'))
